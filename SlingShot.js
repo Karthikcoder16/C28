28d44e8e775b00397f7b0a6616eb21524f43b24d
Karthikcoder16 <rajkumar.analog@gmail.com>
2020-11-05 18:55:17 +0530

    added SlingShot

---
+++ SlingShot.js
@@ -0,0 +1,27 @@
+class SlingShot{
+    constructor(bodyA, newPoint){
+        var options = {
+            bodyA: bodyA,
+            pointB: newPoint,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+    fly(){
+        this.sling.bodyA=null;
+    }
+    
+}
+
